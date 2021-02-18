7d2ad0c79060b46acd1b166e9108a86cfbd95e71
teskrams <teskrams@stud.ntnu.no>
2021-02-18 14:09:04 +0100

    fiks feil i models, og link up med databasen

--- app/models.py
+++ app/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-class Item(models.Model):
-	itemID = models.IntegerField()
-	name = models.CharField(max_lenght=100)
-	value = models.IntegerField()
-	estateID = models.ForeignKey(Estate, on_delete=models.CASCADE)
-	status = models.CharField(max_lenght=100)
-	owner_username = models.ForeignKey(User, on_delete=models.NULL)
-
 class Estate(models.Model):
 	estateID = models.IntegerField()
-	name = models.CharField(max_lenght=100)
+	name = models.CharField(max_length=100)
+
+class Item(models.Model):
+	itemID = models.IntegerField()
+	name = models.CharField(max_length=100)
+	value = models.IntegerField()
+	estateID = models.ForeignKey(Estate, on_delete=models.CASCADE)
+	status = models.CharField(max_length=100)
+	owner_username = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Comment(models.Model):
 	initial_time = models.DateTimeField(default=timezone.now)
@@ -22,10 +22,10 @@ class Comment(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Wish(models.Model):
-	itemID = models.ForeignKey(Item, on_delete=CASCADE)
-	username = models.ForeignKey(User, on_delete=CASCADE)
-	wish = models.CharField(max_lenght=100)
+	itemID = models.ForeignKey(Item, on_delete=models.CASCADE)
+	username = models.ForeignKey(User, on_delete=models.CASCADE)
+	wish = models.CharField(max_length=100)
 
 class Participate(models.Model):
-	username = models.ForeignKey(User, on_delete=CASCADE)
-	estateID = models.ForeignKey(Estate, on_delete=CASCADE)
+	username = models.ForeignKey(User, on_delete=models.CASCADE)
+	estateID = models.ForeignKey(Estate, on_delete=models.CASCADE)
