e69abc64699f455b7d25c108b8f18ddc68c6b6fd
haakonrj <haakonrj@ntnu.no>
2021-03-22 13:05:31 +0100

    Fikset DetailView av dodsbo

--- dodsbo/urls.py
+++ dodsbo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import EstateListView, EstateDetailView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='dodsbo-home'),
-    path('info/', views.info, name='dodsbo-info'),
+    path('estate/<int:pk>/', EstateDetailView.as_view(), name='estate-detail'),
+    path('estates/', EstateListView.as_view(), name='dodsbo-info'),
 ]
