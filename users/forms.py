edb65fd38db9f4b1a8bbfda8d833c93ac4d8db86
haakonrj <haakonrj@ntnu.no>
2021-04-03 04:24:00 +0200

    Styled logout page

--- users/forms.py
+++ users/forms.py
@@ -10,7 +10,8 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name',
+                  'email', 'password1', 'password2']
 
 
 class VoteForm(forms.Form):
